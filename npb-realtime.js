530537741bce122f81a378349fa46342566aa446
tzchen050327-glitch <tzchen050327@gmail.com>
2026-09-14 08:29:53 +0800

    Prepare NPB shared Realtime client for v2.72

---
+++ npb-realtime.js
@@ -0,0 +1,5 @@
+(() => {
+  const VERSION = 'v2.72';
+  const SUPABASE_URL = 'https://kjndnsztbcpmkhictjkr.supabase.co';
+  const ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJIUzI1NiIsInR5cCI6IkpXVCJ9';
+})();
