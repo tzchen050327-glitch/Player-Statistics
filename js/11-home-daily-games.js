b8a7c062895bfe201667a5a1d72059704a49d8e2
tzchen050327-glitch <tzchen050327@gmail.com>
2026-09-20 18:04:35 +0800

    Read NPB home cards from shared live cache

--- js/11-home-daily-games.js
+++ js/11-home-daily-games.js
@@ -223,13 +223,11 @@
     }
 
     async function leagueDailyGamesRequest(league, date) {
-      const requestUrl = league === 'NPB'
-        ? NPB_GAMES_API_URL
-        : league === 'KBO'
-          ? KBO_GAMES_API_URL
-          : league === 'MLB'
-            ? LEAGUE_GAMES_B_API_URL
-            : LEAGUE_GAMES_A_API_URL;
+      const requestUrl = league === 'KBO'
+        ? KBO_GAMES_API_URL
+        : league === 'MLB'
+          ? LEAGUE_GAMES_B_API_URL
+          : LEAGUE_GAMES_A_API_URL;
       const response = await fetch(requestUrl, {
         method:'POST',
         headers:{ 'content-type':'application/json' },
